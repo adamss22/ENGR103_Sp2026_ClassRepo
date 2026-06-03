343a9b6e78af91c94a7d72aa0e942f913b09f492
Caiden Larsen <larsecai@oregonstate.edu>
2026-06-03 12:42:26 -0700

    Add contributor file for larsecai

---
+++ contributors/larsecai.py
@@ -0,0 +1,9 @@
+#Contributor: Caiden Larsen
+#ONID: larsecai@oregonstate.edu
+#Term: Spring 2026
+
+def introduce():
+    print("Hi, I'm Caiden Larsen, ONID larsecai@oregonstate.edu")
+    
+if __name__ == "__main__":
+    introduce()
