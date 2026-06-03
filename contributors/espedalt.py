c6310c548f5c9d67d0bf61a94184dc99bd869a2b
TaylorEspedal <espedalt@oregonstate.edu>
2026-06-03 13:03:07 -0700

    Add contributor file for espedalt

---
+++ contributors/espedalt.py
@@ -0,0 +1,9 @@
+# Contributor: TaylorEspedal
+# ONID: espedalt
+# Term: Spring 2026
+
+def introduce():
+    print("Hi, I'm TaylorEspedal, ONID espedalt.")
+    
+if __name__ == "__main__":
+    introduce()
