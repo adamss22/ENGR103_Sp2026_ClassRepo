3d942a44e6b7c55cbbdf53bf6ece59c0e8b7ccdc
chris-nate-213 <christophersonnate@gmail.com>
2026-06-03 13:16:47 -0700

    Add contributor file for christn2

---
+++ contributors/christn2.py
@@ -0,0 +1,7 @@
+# Contributor : Nathaniel Christopherson
+# ONID : christn2
+# Term : Spring 2026
+def introduce():
+    print (" Hi , I ’ m Nathaniel Christopherson, ONID christn2.")
+if __name__ == "__main__":
+    introduce()
