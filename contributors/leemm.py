a9f457ab85fcf4aa642a6e1bf7f0d2412466183e
Emma <leemm@oregonstate.edu>
2026-06-03 13:18:03 -0700

    Add contributor filed for leemm

---
+++ contributors/leemm.py
@@ -0,0 +1,11 @@
+# Contributor : <Emma Le >
+# ONID : < leemm >
+# Term : < Spring 2026 >
+
+def introduce () :
+    print (" Hi , I’m < Emma > , ONID < leemm >.")
+
+if __name__ == " __main__ ":
+    introduce ()
+introduce()
+
