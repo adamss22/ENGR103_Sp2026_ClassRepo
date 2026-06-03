4a6fb3902c3df3f59dd219a3619f866971fd9da0
ScarlettRedMiku39 <ivess@oregonstate.edu>
2026-06-03 13:13:02 -0700

    Add contributor file for ivess

---
+++ contributors/ivess.py
@@ -0,0 +1,9 @@
+# Contributor : 	< Scarlett Ives >
+# ONID : 			< ivess >
+# Term : 			< Spring 2026 >
+
+def introduce ():
+    print (" Hi , I ’ m < Scarlett Ives > , ONID < ivess >.")
+    
+if __name__ == "__main__":
+    introduce () 
