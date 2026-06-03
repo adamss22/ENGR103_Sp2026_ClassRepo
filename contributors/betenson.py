a5092b5311449fee6e1386e875f34f83bad036a6
norabetenson <betenson@oregonstate.edu>
2026-06-03 13:21:35 -0700

    Add contributor file for betenson

---
+++ contributors/betenson.py
@@ -0,0 +1,10 @@
+# Contributor : <Nora Betenson >
+# ONID : <betenson>
+# Term : <Sp 2026>
+def introduce () :
+    print (" Hi , I'm Nora Betenson , ONID betenson.")
+if __name__ == " __introduce__ ":
+    introduce ()
+introduce()
+
+
