8c752d93d40718eafe78ed8033494b05bb721b93
DoggieDoo47 <nemackie@gmail.com>
2026-06-03 13:08:30 -0700

    Add contributor file for mackien

---
+++ contributors/mackien.py
@@ -0,0 +1,9 @@
+# Contributor: Noel Mackie
+# ONID: mackien
+# Term: Spring 2026
+
+def introduce():
+    print("Hi, I'm Noel Mackie, ONID mackien")
+    
+if __name__ == "__main__":
+    introduce()
