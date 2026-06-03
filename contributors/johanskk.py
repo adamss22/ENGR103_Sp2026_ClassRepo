9482b0bb239d9d86c66fcfc89e014768d3f9c574
Kieran Johanski <johanskk@oregonstate.edu>
2026-06-03 13:04:56 -0700

    Add contributor file for johanskk

---
+++ contributors/johanskk.py
@@ -0,0 +1,9 @@
+#Contributor: Kieran Johanski
+#ONID: johanskk
+#Term: Spring 2026
+
+def introduce():
+    print("Hi, I'm Kieran Johanski, ONID johanskk")
+    
+if __name__ == "__main__":
+    introduce()
