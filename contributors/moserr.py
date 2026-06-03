a206a226a7c91dac568bb89e78e4492f54762550
Rece Moser <moserr@oregonstate.edu>
2026-06-03 13:09:34 -0700

    Add contributor file for moserr

---
+++ contributors/moserr.py
@@ -0,0 +1,9 @@
+# Contributor : < Rece Moser >
+# ONID : < moserr >
+# Term : < Spring 2026 >
+
+def introduce() :
+    print(" Hi , I’m < Rece Moser > , ONID < moserr >.")
+    
+if __name__ == "__main__":
+    introduce()
