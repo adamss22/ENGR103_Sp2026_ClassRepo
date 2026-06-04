2aab3fbec8d3019ea8c2acb408cc9e833f4929f5
Bret Bosworth <Bretbosworth@outlook.com>
2026-06-04 08:57:17 -0700

    Add contributor file for boswortb

---
+++ contributors/boswortb.py
@@ -0,0 +1,8 @@
+# Contributor: <Bret Bosworth>
+# ONID: <Boswortb>
+# Term: <Spring, 2026>
+def introduce():
+    print("Hi, I'm Bret Bosworth, boswortb")
+if __name__ == "__main__":
+    introduce()
+    
